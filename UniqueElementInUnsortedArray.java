9ae01ba81154cbc800c591144328c2543f95da05
Aditya yadav <adityayadaw42@gmail.com>
2025-08-31 00:13:31 +0530

    unique element who occur once in unsorted array by xor method optimal solution

--- UniqueElementInUnsortedArray.java
+++ UniqueElementInUnsortedArray.java
@@ -2,19 +2,27 @@ public class UniqueElementInUnsortedArray {
 
     static int unique(int arr[]){
         //Brute force apporach
+        // for(int i=0;i<arr.length;i++){
+        //     int count=0;
+        //     for(int j=0;j<arr.length;j++){
+        //         if(arr[i]==arr[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count==1){
+        //         return arr[i];
+        //     }
+        // }
+        // return 0;
+        //XOR operator = same 0 and different me element
+
+        int xor=0;
         for(int i=0;i<arr.length;i++){
-            int count=0;
-            for(int j=0;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    count++;
-                }
-            }
-            if(count==1){
-                return arr[i];
-            }
+            xor=xor^arr[i];
         }
-        return 0;
+        return xor;
     }
+
     public static void main(String[] args) {
         int arr[]={2,2,4,3,5,4,5};
         System.out.println(unique(arr));
