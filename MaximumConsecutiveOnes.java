b39caacbbbb2da376fa171cd234fd244f15665ae
Aditya yadav <adityayadaw42@gmail.com>
2025-08-30 18:15:28 +0530

    maximum consecutive ones

---
+++ MaximumConsecutiveOnes.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class MaximumConsecutiveOnes {
+
+    static int maximumOnes(int arr[]) {
+        int count = 0;
+        ArrayList <Integer> list=new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                list.add(count);
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+        list.add(count);
+        System.out.println(list);
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<list.size();i++){
+            if(list.get(i)>max){
+                max=list.get(i);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 1, 0, 1, 1, 1, 1,1, 0, 1, 1, 1,1,1,1,1 };
+        System.out.println(maximumOnes(arr));
+    }
+}
