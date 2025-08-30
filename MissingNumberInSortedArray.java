521e04d1960c83f97dea7896b79f0e2a4e665294
Aditya yadav <adityayadaw42@gmail.com>
2025-08-31 00:20:26 +0530

    if n natural number will greater than int dataTypes then it will throw error i have to fix it

--- MissingNumberInSortedArray.java
+++ MissingNumberInSortedArray.java
@@ -8,6 +8,8 @@ public class MissingNumberInSortedArray {
             s+=arr[i];
         }
         return sum-s;
+
+
 }
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7,8,10};
