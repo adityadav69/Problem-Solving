d20e349534ab611fcc80f2d49453bc0750dad74e
Aditya yadav <adityayadaw42@gmail.com>
2025-08-31 00:11:39 +0530

    Unique element in unsorted array who occur once by brute force

---
+++ UniqueElementInUnsortedArray.java
@@ -0,0 +1,22 @@
+public class UniqueElementInUnsortedArray {
+
+    static int unique(int arr[]){
+        //Brute force apporach
+        for(int i=0;i<arr.length;i++){
+            int count=0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count==1){
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int arr[]={2,2,4,3,5,4,5};
+        System.out.println(unique(arr));
+    }
+}
