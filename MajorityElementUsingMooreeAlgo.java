ebdbdaa12565c27d97ca34b77b197b4294320696
Aditya yadav <adityayadaw42@gmail.com>
2025-09-03 00:01:01 +0530

    Mojority Element Find By Mooree Algo

--- MajorityElementUsingMooreeAlgo.java
+++ MajorityElementUsingMooreeAlgo.java
@@ -1,5 +1,13 @@
 public class MajorityElementUsingMooreeAlgo{
+
+    static int findMajority(int arr[]){
+        int count=0;
+        int el=0;
+
+        return -1;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int arr[]={7,7,5,7,5,1,5,7,5,5,7,7,5,5,5,5};
+        System.out.println(findMajority(arr));        
     }
 }
