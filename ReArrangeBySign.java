28efa782489690bdc45911df13ede7ee28c38202
Aditya yadav <adityayadaw42@gmail.com>
2025-09-03 23:15:41 +0530

    Re arrange array element by sign

---
+++ ReArrangeBySign.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class ReArrangeBySign {
+
+    static void reArrange(int arr[]) {
+        ArrayList <Integer> positive=new ArrayList<>();
+        ArrayList <Integer> negative=new ArrayList<>();
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                negative.add(arr[i]);
+            }
+            else{
+                positive.add(arr[i]);
+            }
+        }
+
+
+        System.out.println(positive);
+        System.out.println(negative);
+
+        for(int i=0;i<arr.length/2;i++){
+            arr[i*2]=positive.get(i);
+            arr[i*2+1]=negative.get(i);
+        }
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 1, -2, -5, 2, -4 };
+        reArrange(arr);
+
+    }
+}
