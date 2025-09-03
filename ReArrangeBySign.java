ecbedaa6ff72c153c6fa75ca25561d5dde159933
Aditya yadav <adityayadaw42@gmail.com>
2025-09-03 23:17:38 +0530

    Re arrange element by sign better solution

--- ReArrangeBySign.java
+++ ReArrangeBySign.java
@@ -3,30 +3,48 @@ import java.util.ArrayList;
 public class ReArrangeBySign {
 
     static void reArrange(int arr[]) {
-        ArrayList <Integer> positive=new ArrayList<>();
-        ArrayList <Integer> negative=new ArrayList<>();
+        // ArrayList <Integer> positive=new ArrayList<>();
+        // ArrayList <Integer> negative=new ArrayList<>();
 
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]<0){
+        //         negative.add(arr[i]);
+        //     }
+        //     else{
+        //         positive.add(arr[i]);
+        //     }
+        // }
+
+
+        // System.out.println(positive);
+        // System.out.println(negative);
+
+        // for(int i=0;i<arr.length/2;i++){
+        //     arr[i*2]=positive.get(i);
+        //     arr[i*2+1]=negative.get(i);
+        // }
+
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.print(arr[i]+" ");
+        // }
+
+        int np=1;
+        int pp=0;
+        int arr2[]=new int[arr.length];
         for(int i=0;i<arr.length;i++){
             if(arr[i]<0){
-                negative.add(arr[i]);
+                arr2[np]=arr[i];
+                np=np+2;
             }
             else{
-                positive.add(arr[i]);
+                arr2[pp]=arr[i];
+                pp+=2;
             }
         }
-
-
-        System.out.println(positive);
-        System.out.println(negative);
-
-        for(int i=0;i<arr.length/2;i++){
-            arr[i*2]=positive.get(i);
-            arr[i*2+1]=negative.get(i);
-        }
-
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
+
     }
 
     public static void main(String[] args) {
