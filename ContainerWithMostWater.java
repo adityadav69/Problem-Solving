a26af1d71d0a400e2c70e1b59fcc88c9c980a6c1
Aditya yadav <adityayadaw42@gmail.com>
2025-09-11 21:06:47 +0530

    Container with most water problem

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            
+            maxArea = Math.max(maxArea, currentArea);
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={2,2,43,2,43,2,2};
+        System.out.println(maxArea(arr));
+    }
+}
