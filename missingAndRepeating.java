64277f5af207dc00acd169476521c724cd4aa9aa
Aditya yadav <adityayadaw42@gmail.com>
2025-09-13 22:33:03 +0530

    Find Missing and repeating number in 0n and 0 1 sc

--- missingAndRepeating.java
+++ missingAndRepeating.java
@@ -2,24 +2,51 @@ import java.util.ArrayList;
 
 public class missingAndRepeating {
     
+    //HashArray method
+    // static ArrayList<Integer> FindMissing(int arr[]){
+    //     ArrayList<Integer> ans=new ArrayList<>();
+    //     int arr2[]=new int[arr.length+1];
+    //     for(int i=0;i<arr.length;i++){
+    //         arr2[arr[i]]++;
+    //     }
+    //     int missing=-1;
+    //     int repeating=-1;
+    //     for(int i=0;i<arr2.length;i++){
+    //         if(arr2[i]==2){
+    //             repeating=i;
+    //         }
+    //         else if(arr2[i]==0){
+    //             missing=i;
+    //         }
+    //     }
+    //     ans.add(repeating);
+    //     ans.add(missing);
+    //     return ans;
+    // }
+
     static ArrayList<Integer> FindMissing(int arr[]){
         ArrayList<Integer> ans=new ArrayList<>();
-        int arr2[]=new int[arr.length+1];
+
+        long n=arr.length;
+
+        long sn=n*(n+1)/2;
+        long sn2=n*(n+1)*((2*n)+1)/6;
+
+        long s=0;
+        long s2=0;
         for(int i=0;i<arr.length;i++){
-            arr2[arr[i]]++;
+            s=s+(long)arr[i];
+            s2=s2+(long)arr[i]*(long)arr[i];
         }
-        int missing=-1;
-        int repeating=-1;
-        for(int i=0;i<arr2.length;i++){
-            if(arr2[i]==2){
-                repeating=i;
-            }
-            else if(arr2[i]==0){
-                missing=i;
-            }
-        }
-        ans.add(repeating);
-        ans.add(missing);
+        long val1=s-sn;
+        long val2=s2-sn2;
+        val2=val2/val1;
+        long x=(val1+val2)/2;
+        long y=x-val1;
+        x=(int)x;
+        y=(int)y;
+        ans.add((int)x);
+        ans.add((int)y);
         return ans;
     }
     public static void main(String args[]){
