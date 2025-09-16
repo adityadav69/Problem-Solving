5bd2f4cc6287d26bbfd1abc31ce8b2da49199520
Aditya yadav <adityayadaw42@gmail.com>
2025-09-16 22:10:36 +0530

    optimul solution of maximul product array

--- MaximumProductArray.java
+++ MaximumProductArray.java
@@ -1,24 +1,38 @@
 public class MaximumProductArray {
 
     static int findMaxProduct(int arr[]) {
-        int maxProduct = Integer.MIN_VALUE;
-        int product=1;
-        for (int i = 0; i < arr.length; i++) {
-            product=1;
-            for (int j = i; j < arr.length; j++) {
-               product=product* arr[j];
-                if (product > maxProduct) {
-                    maxProduct = product;
-                }
-            }
+        // int maxProduct = Integer.MIN_VALUE;
+        // int product=1;
+        // for (int i = 0; i < arr.length; i++) {
+        //     product=1;
+        //     for (int j = i; j < arr.length; j++) {
+        //        product=product* arr[j];
+        //         if (product > maxProduct) {
+        //             maxProduct = product;
+        //         }
+        //     }
 
+        // }
+        // return maxProduct;
+        
+        // optimul solution
+        int max=Integer.MIN_VALUE;
+        int n=arr.length;
+        int prefix=1;
+        int suffix=1;
+        for(int i=0;i<arr.length;i++){
+            if(prefix==0)prefix=1;
+            if(suffix==0)suffix=1;
+            prefix=prefix*arr[i];
+            suffix=suffix*arr[(n-1)-i];
+            System.out.println("Suffix is "+suffix);
+            max=Math.max(max,Math.max(prefix,suffix));
         }
-        return maxProduct;
-
+        return max;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 2, 3, -2, 4 };
+        int arr[] = { 1,0,-1,2,3,-5,-2 };
         System.out.println(findMaxProduct(arr));
     }
 }
