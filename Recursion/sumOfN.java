e4df56384130cb3140f76b60982c8510a72c37e5
Aditya yadav <adityayadaw42@gmail.com>
2025-09-18 22:33:18 +0530

    organise file

---
+++ Recursion/sumOfN.java
@@ -0,0 +1,12 @@
+public class sumOfN{
+
+
+    static int sum(int n){
+        if(n==0)return 0;
+        int sum=n+sum(n-1);
+        return sum;
+    }
+    public static void main(String args[]){
+        System.out.println(sum(3));
+    }
+}
