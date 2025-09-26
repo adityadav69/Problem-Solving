fe7dabd36e509845cfea7d7018654cee265dda5a
Aditya yadav <adityayadaw42@gmail.com>
2025-09-26 11:18:43 +0530

    recursion

---
+++ Recursion/PrintNumberInIncreasingOrder.java
@@ -0,0 +1,13 @@
+public class PrintNumberInIncreasingOrder {
+    static void printNumber(int n){
+        if(n==1){
+            System.out.print(1);
+            return;
+        }
+        System.out.print(n+" , ");
+        printNumber(n-1);
+    }
+    public static void main(String[] args) {
+        printNumber(10);
+    }
+}
