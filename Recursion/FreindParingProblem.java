edb1c297eecf47be8d4b3adbe71f4b93e5906b15
Aditya yadav <adityayadaw42@gmail.com>
2025-09-30 22:32:12 +0530

    recursion

---
+++ Recursion/FreindParingProblem.java
@@ -0,0 +1,15 @@
+public class FreindParingProblem {
+
+    static int countPair(int n){
+        if(n==1 || n==2){
+            return n;
+        }
+        int single=countPair(n-1);
+        int pair=countPair(n-2);
+        int pairWays=(n-1)*pair;
+        return single+pairWays;
+    }
+    public static void main(String[]  args) {
+        System.out.println(countPair(3));
+    }
+}
